b40de99b0db38b73c0b7f6ccf4d1141328bf107f
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2017-11-12 14:31:05 +0100

    Re-add 'tee | $logfile' to generate test statistics

--- roles/contrail-unittests/files/contrail-unittests-job.sh
+++ roles/contrail-unittests/files/contrail-unittests-job.sh
@@ -202,9 +202,8 @@ function run_unittest() {
     fi
 
     logfile=$WORKSPACE/scons_test.log
-    echo scons -k -j $SCONS_JOBS $UNIT_TESTS
-    echo "Check job-output.txt.gz/ARA for the full log." > $logfile
-    scons -k --debug=explain -j $SCONS_JOBS $UNIT_TESTS
+    echo scons --debug=explain -k -j $SCONS_JOBS $UNIT_TESTS
+    scons -k --debug=explain -j $SCONS_JOBS $UNIT_TESTS | tee $logfile
     exit_status=$?
     analyze_test_results $logfile
 
