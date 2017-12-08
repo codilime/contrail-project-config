95b50d14491c835256d58d51bef7669aaf8c7db5
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2017-12-08 11:33:01 +0100

    Update manifest translator to inject contrail-packaging
    Until contrail-packaging lands in contrail-vnc we need to inject it into
    manifest.xml so that CentOS builds work. Add that to manifest
    translator.
    
    Also, update manifest translator to use '_projects' variable from zuul
    ('project' list is being deprecated), and raise a useful error when
    project is missing.

--- roles/repo-sandbox-prepare/files/manifest_translator.py
+++ roles/repo-sandbox-prepare/files/manifest_translator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from __future__ import print_function
+import os
 import sys
 import yaml
 import lxml
@@ -16,10 +17,13 @@ def del_node(node):
     node.getparent().remove(node)
 
 def get_project(zuul_var, short_name):
-    for p in zuul_var['projects']:
-        if p['short_name'] == short_name:
-            return p
-    return None
+    for project, data in zuul_var['_projects'].items():
+        if data['short_name'] == short_name:
+            return data
+
+    msg = "Zuul does not know about project {}.\n".format(short_name)
+    msg += "Make sure it is defined in ``required-projects'' for this job."
+    raise RuntimeError(msg)
 
 with open(zuul_var_path, 'r') as zuul_var_file:
   zuul_var = yaml.load(zuul_var_file)
@@ -47,9 +51,14 @@ for remote in remotes.values():
 for project in manifest.xpath('//project'):
     name = project.attrib['name']
     zuul_project = get_project(zuul_var, name)
-    head = subprocess.check_output(['git', 'symbolic-ref', 'HEAD'], cwd=zuul_var['executor']['work_root'] + '/' + zuul_project['src_dir'])
     project.attrib['remote'] = zuul_project['canonical_hostname']
-    project.attrib['revision'] = head[:-1]
+
+# XXX(kklimonda): Remove after contrail-packaging has been added to contrail-vnc
+if not manifest.find('//project[@name="contrail-packaging"]'):
+    project = get_project(zuul_var, "contrail-packaging")
+    contrail_packaging = etree.Element("project", name=project['short_name'], remote=project['canonical_hostname'], path='tools/packaging')
+    contrail_packaging.tail = '\n'
+    manifest.getroot().append(contrail_packaging)
 
 with open(manifest_path, 'w') as manifest_file:
     manifest_file.write(dump_xml(manifest))
